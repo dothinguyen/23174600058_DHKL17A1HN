600bd8e8804bc05bf48f8eed8f1b6e443930f09c
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-10-30 12:55:03 +0700

    Update and rename 1.4.py to chuong5_1.4.py

--- chuong5_1.4.py
+++ chuong5_1.4.py
@@ -1,8 +1,6 @@
 a = float(input("Nhập độ dài cạnh a: "))
 b = float(input("Nhập độ dài cạnh b: "))
 c = float(input("Nhập độ dài cạnh c: "))
-
 p = (a + b + c) / 2
 S = (p*(p-a)*(p-b)*(p-c)) ** 0.5
-
 print(f"Diện tích tam giác là {S}")
