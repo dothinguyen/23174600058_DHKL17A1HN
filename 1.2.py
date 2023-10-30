40dea1614357c414a080bc92170927395fb2b1c5
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-10-30 09:53:28 +0700

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,9 @@
+Ten_hang = input("Nhập tên hàng: ")
+So_luong = int(input("Nhập số lượng: "))
+Don_gia = float(input("Nhập đơn giá: "))
+Tien_phai_tra_vnđ = So_luong * Don_gia
+print("Tên hàng:", Ten_hang)
+print("Số lượng:", So_luong)
+print("Đơn giá:", Don_gia)
+print("Tiền phải trả:", Tien_phai_tra_vnđ) 
+
---
+++ 1.4.py
@@ -0,0 +1,9 @@
+65
+a = float(input("Nhập độ dài cạnh a: "))
+b = float(input("Nhập độ dài cạnh b: "))
+c = float(input("Nhập độ dài cạnh c: "))
+
+p = (a + b + c) / 2
+S = (p*(p-a)*(p-b)*(p-c)) ** 0.5
+
+print(f"Diện tích tam giác là {S}")
