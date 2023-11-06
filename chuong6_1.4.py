74522f91089cec20e2ee2b3ecc48c641251dd2e1
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-11-06 19:52:20 +0700

    Add files via upload

---
+++ chuong6_1.4.py
@@ -0,0 +1,11 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 0
+0
+>>> result = (5 - 3) // 2
+>>> print(result)
+1
+>>> -(8 - 3*2 - 1) + 1
+0
+>>> print(-(8 - 3*2 - 1) + 1)
+0
