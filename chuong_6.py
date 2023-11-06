a925b9ae3428c05dcbd76f44f368eb013915c00e
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-11-06 19:06:18 +0700

    Add files via upload

---
+++ chuong_6.py
@@ -0,0 +1,14 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 0
+0
+>>> x=eval(input("Nhập x : "))
+Nhập x : 5
+>>> y=eval(input("Nhập y : "))
+Nhập y : 10
+>>> tong = x + y
+>>> hieu = x - y
+>>> tich = x*y
+>>> thuong = x/y
+>>> print("Tổng = %d, hiệu = %d, tích = %d, thương = %0.1f"%(tong, hieu, tich, thuong))
+Tổng = 15, hiệu = -5, tích = 50, thương = 0.5
