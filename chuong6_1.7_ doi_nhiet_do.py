e5ef09656896e8f57e534065656b0ec3985f4a04
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-11-06 20:48:54 +0700

    chuong6_1.7_ doi_nhiet_do.py

--- chuong6_1.7_ doi_nhiet_do.py
+++ chuong6_1.7_ doi_nhiet_do.py
@@ -1,9 +1,10 @@
 Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 0
 0
->>> do_C = float(input("Nhập nhiệt độ Celsius: "))
-... 
-... do_F = 9/5 * do_C + 32
-... 
-... print("Nhiệt độ Fahrenheit tương ứng là:", do_F)
+0
+do_C = float(input("Nhập nhiệt độ Celsius: "))
+Nhập nhiệt độ Celsius: 27
+do_F = 9/5 * do_C + 32
+print("Nhiệt độ Fahrenheit tương ứng là:", do_F)
+Nhiệt độ Fahrenheit tương ứng là: 80.6
+
