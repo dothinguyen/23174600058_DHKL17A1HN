0b10491d08af6321bdeca630ab8bcd97d265e7f0
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-11-06 20:46:53 +0700

    Add files via upload

---
+++ chuong6_1.8.py
@@ -0,0 +1,24 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 0
+0
+>>> s1 = input("Nhập chuỗi s1: ")
+Nhập chuỗi s1: hello
+>>> s2 = input("Nhập chuỗi s2: ")
+Nhập chuỗi s2: python
+>>> s3 = input("Nhập chuỗi s3: ")
+Nhập chuỗi s3: programming language
+>>> index = int(input("Nhập chỉ mục index: "))
+Nhập chỉ mục index: 2
+>>> length_s1 = len(s1)
+>>> length_s2 = len(s2)
+>>> length_s3 = len(s3)
+>>> print("Chiều dài của chuỗi s1 là:", length_s1)
+Chiều dài của chuỗi s1 là: 5
+>>> print("Chiều dài của chuỗi s2 là:", length_s2)
+Chiều dài của chuỗi s2 là: 6
+>>> print("Chiều dài của chuỗi s3 là:", length_s3)
+Chiều dài của chuỗi s3 là: 20
+>>> s4 = s1[index:]
+>>> print(f"Chuỗi con s4 từ index đến hết chuỗi s1 là: {s4}")
+Chuỗi con s4 từ index đến hết chuỗi s1 là: llo
