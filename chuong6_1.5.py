3197b6497baa1d3a0d28d5c6f43495aebc84e25c
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-11-06 20:09:20 +0700

    Add files via upload

---
+++ chuong6_1.5.py
@@ -0,0 +1,11 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 0
+0
+>>> so_luong = int(input("Nhập số lượng: "))
+Nhập số lượng: 100
+>>> don_gia = int(input("Nhập đơn giá: "))
+Nhập đơn giá: 5000
+>>> thanh_tien = so_luong * don_gia
+>>> print("Thành tiền =", thanh_tien)
+Thành tiền = 500000
