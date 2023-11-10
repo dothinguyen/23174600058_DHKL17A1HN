114a80e7d47d1754dce5eeb2f5e0c2b9080c5241
dothinguyen <148058042+dothinguyen@users.noreply.github.com>
2023-11-10 19:14:32 +0700

    Add files via upload

---
+++ chuong8_8.11.py
@@ -0,0 +1,11 @@
+Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 0
+0
+>>> x = float(input("Nhập x: "))
+Nhập x: 4
+>>> n = int(input("Nhập n: "))
+Nhập n: 3
+>>> result = (x**2 + x + 1)**n + (x**2 - x + 1)**n
+>>> print("Kết quả của biểu thức A là:", result)
+Kết quả của biểu thức A là: 11458.0
---
+++ chuong8_8.9.py
@@ -0,0 +1,11 @@
+import time
+
+n = int(input("Nhập số nguyên n: "))
+
+for i in range(n, -1, -1):
+    print(i)
+    time.sleep(1)
+    
+print("Start !!!")
+    
+
