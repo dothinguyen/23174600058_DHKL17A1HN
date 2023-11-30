e0fb88af966d4d3024adb1c794d6c6dfb6cf1b3e
Đỗ Thị Nguyên_32 <148058042+dothinguyen@users.noreply.github.com>
2023-11-30 17:32:34 +0700

    Update chuong8_8.2.py

--- chuong8_8.2.py
+++ chuong8_8.2.py
@@ -1,10 +1,3 @@
-Python 3.11.6 (tags/v3.11.6:8b6ee5b, Oct  2 2023, 14:57:12) [MSC v.1935 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 0
-0
->>> number = float(input("Nhập x: "))
-Nhập x: -10
->>> absolute_value = abs(number)
->>> result = f"|{number}|={absolute_value}"
->>> print(result)
-|-10.0|=10.0
+x = int(input("nhập giá trị của x:"))
+abs_x = abs(x)
+print("giá trị tuyệt đối của x là:",abs_x)
